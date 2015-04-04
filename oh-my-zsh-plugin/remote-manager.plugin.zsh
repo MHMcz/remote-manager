89f1c104a1ee39a78fde144167968dfa6889cc79
Jan Markup <mhmcze@gmail.com>
2015-04-04 20:01:07 +0200

    add license text to ohmyzsh plugin

--- oh-my-zsh-plugin/remote-manager.plugin.zsh
+++ oh-my-zsh-plugin/remote-manager.plugin.zsh
@@ -1,3 +1,8 @@
+# Copyright © 2014 Jan Markup <mhmcze@gmail.com>
+# This work is free. You can redistribute it and/or modify it under the
+# terms of the Do What The Fuck You Want To Public License, Version 2,
+# as published by Sam Hocevar. See the COPYING file for more details.
+
 . "$HOME/.remote-manager/config"
 
 get_group() {
